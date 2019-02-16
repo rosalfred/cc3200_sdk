033ab3824ffe68f47bac8289f41623e6338c5a7a
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2019-02-16 16:33:01 +0100

    Enable Recursive Mutexes

--- third_party/FreeRTOS/source/FreeRTOSConfig.h
+++ third_party/FreeRTOS/source/FreeRTOSConfig.h
@@ -91,7 +91,7 @@
 #define configIDLE_SHOULD_YIELD			0
 #define configUSE_CO_ROUTINES 			0
 #define configUSE_MUTEXES				1
-#define configUSE_RECURSIVE_MUTEXES		0
+#define configUSE_RECURSIVE_MUTEXES		1
 #define configCHECK_FOR_STACK_OVERFLOW	1
 #define configUSE_QUEUE_SETS			1
 #define configUSE_COUNTING_SEMAPHORES	1
@@ -136,8 +136,8 @@ version. */
 #define configUSE_PORT_OPTIMISED_TASK_SELECTION 1
 
 #ifdef __ICCARM__
-	void vAssertCalled( const char *pcFile, unsigned long ulLine );
-	#define configASSERT( x ) if( x == 0 ) vAssertCalled( __FILE__, __LINE__ );
+    void vAssertCalled( const char *pcFile, unsigned long ulLine );
+    #define configASSERT( x ) if( x == 0 ) vAssertCalled( __FILE__, __LINE__ );
 #endif
 
 #if defined(ccs)
